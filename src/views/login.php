8d098b9c2597c42aea1c4ca69ff9d09beba0bcbf
G0shk1 <Mauricio.291202@gmail.com>
2021-11-12 01:06:20 -0600

    solved login

--- src/views/login.php
+++ src/views/login.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<h1>Iniciar Sesion</h1>
-
-<form id="sesion_data" method="POST" 
-      enctype="application/x-www-form-urlencoded">
-	<input type="text" />
-	<input type="password" />
-	<button type="submit" onsubmit="log_in">Entrar</button>
-</div>
-
-<script>
-	function log_in(e) {
-		e.preventDefault()
-=======
 <main>
 	<h1>Iniciar Sesión</h1>
 	<div class="field">
@@ -44,6 +30,5 @@
 			.then(jresp => {
 				
 			});
->>>>>>> 468f93e19ab0ae12c32e92a979e55b113d2bd94e
 	}
 </script>
